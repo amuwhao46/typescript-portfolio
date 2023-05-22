21dcef16a2c8742a0ef4d303c692d5ad62ec0aa9
Oke Amuwha <amuwhao46@gmail.com>
2023-05-21 22:36:20 -0400

    Sanity blocker, added icons to contact page

--- components/Contacts.tsx
+++ components/Contacts.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { EnvelopeIcon, PhoneIcon } from "@heroicons/react/24/solid";
+import { SocialIcon } from "react-social-icons";
 
 type Props = {};
 
@@ -12,12 +13,22 @@ export default function Contacts({}: Props) {
       <div className="flex flex-col space-y-10">
         <h4 className="text-center text-4xl font-semibold">Get in touch</h4>
         <div className="flex items-center justify-center space-x-5">
-          <PhoneIcon className="mr-2 inline-block h-7 w-7 animate-pulse text-gray-600" />
-          <p className="text-2xl">+1 (248) 495-7743</p>
-        </div>
-        <div className="flex items-center justify-center space-x-5">
           <EnvelopeIcon className="mr-2 inline-block h-7 w-7 animate-pulse text-gray-600" />
           <p className="text-2xl">amuwhao46@gmail.com</p>
+        </div>
+        <div className="flex flex-row justify-between">
+          <SocialIcon
+            bgColor="transparent"
+            fgColor="white"
+            url="https://www.linkedin.com/in/oke-oghene-amuwha-1b1489203/"
+            style={{ height: 50, width: 50 }}
+          />
+                    <SocialIcon
+            bgColor="transparent"
+            fgColor="white"
+            url="https://github.com/amuwhao46"
+            style={{ height: 50, width: 50 }}
+          />
         </div>
       </div>
     </div>
--- sanity/schema.ts
+++
@@ -1,8 +0,0 @@
-import { type SchemaTypeDefinition } from 'sanity'
-
-import pageInfo from './schemas/pageInfo'
-
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [pageInfo],
-}
