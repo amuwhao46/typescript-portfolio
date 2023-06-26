6a03663b141a02b2d3dd6fecac697b73f2307073
Oke Amuwha <amuwhao46@gmail.com>
2023-06-26 16:09:34 -0400

    Updated logo for visibility

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,7 +14,7 @@ export default function Header({}: Props) {
     <header>
       <nav className="flex-center fixed inset-x-0 z-50 flex flex-wrap justify-between p-5 shadow-lg backdrop-blur">
         <Link href="#hero">
-          <div className="mr-6 flex flex-shrink-0 items-center space-x-2 rounded-2xl px-2 text-black transition-colors hover:bg-neutral-200 dark:bg-gray-100">
+          <div className="mr-6 flex flex-shrink-0 items-center space-x-2 rounded-2xl px-2">
             <Image src={Logo} width={30} alt="My logo" className="" />
             <span className="text-xl font-bold tracking-tight md:text-3xl">
               Oke Amuwha
